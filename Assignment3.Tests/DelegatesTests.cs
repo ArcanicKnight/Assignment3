afab2d5b456e28171cae96adefc3a34d4f63502c
ArcanicKnight <alsu@itu.dk>
2021-09-24 12:41:28 +0200

    quick fix

--- Assignment3.Tests/DelegatesTests.cs
+++ Assignment3.Tests/DelegatesTests.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Xunit;
 
-namespace BDSA2020.Assignment02.Tests
+namespace BDSA2020.Assignment03.Tests
 {
     public class DelegatesTests
     {
--- Assignment3/Extension.cs
+++ Assignment3/Extension.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace BDSA2020.Assignment02
+namespace BDSA2020.Assignment03
 {
     public static class Extensions
     {
